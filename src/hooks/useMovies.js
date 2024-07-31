519f3d592703b2e65e7e2877a2df123342842651
devzdevelop <devbusinessinfluencer@gmail.com>
2024-07-31 14:53:45 -0500

    used https instead of http for the api request

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,25 +1,27 @@
 import { useState, useEffect } from "react";
-const KEY = "d39fd186"
+const KEY = "d39fd186";
 export function useMovies(query, currentPage, setPageList, pageList) {
-    const [movies, setMovies] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(false);
-    // fetching movie data from OMDb API
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+  // fetching movie data from OMDb API
   useEffect(() => {
     // callback?.();
     const controller = new AbortController();
 
-    const getMovies = async () =>{
+    const getMovies = async () => {
       try {
         setIsLoading(true);
         setError(false);
 
-        if(!query) {
-          setPageList(undefined)
+        if (!query) {
+          setPageList(undefined);
         }
 
-        const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}&page=${currentPage}`, 
-        { signal : controller.signal })
+        const res = await fetch(
+          `https://www.omdbapi.com/?apikey=${KEY}&s=${query}&page=${currentPage}`,
+          { signal: controller.signal }
+        );
 
         const data = await res.json();
 
@@ -27,43 +29,47 @@ export function useMovies(query, currentPage, setPageList, pageList) {
         // console.log(data);
         // console.log('Fetch Movie List Error: ', data.Error || 'No error.');
 
-        if(data.Error === "Too many results." || data.Error === "Movie not found!" || data.Error === 'Incorrect IMDb ID.') {
-          if(data.Error === "Movie not found!") {
+        if (
+          data.Error === "Too many results." ||
+          data.Error === "Movie not found!" ||
+          data.Error === "Incorrect IMDb ID."
+        ) {
+          if (data.Error === "Movie not found!") {
             throw new Error(data.Error);
           }
-          if(data.Error === "Too many results.") {
-            throw new Error("No movies found. Please type in more characters for the movie you are searching for.");
+          if (data.Error === "Too many results.") {
+            throw new Error(
+              "No movies found. Please type in more characters for the movie you are searching for."
+            );
           }
-          if(data.Error === 'Incorrect IMDb ID.') {
-            throw new Error('Search for a movie')
+          if (data.Error === "Incorrect IMDb ID.") {
+            throw new Error("Search for a movie");
           }
-          return
+          return;
         } else {
-          setMovies(data.Search)
+          setMovies(data.Search);
           setError("");
           // console.log('Search Query: ', query)
         }
 
         // calculates the total number of pages based on the amount of fetch results
-        setPageList( Math.floor((data.totalResults / data.Search.length)));
+        setPageList(Math.floor(data.totalResults / data.Search.length));
 
         setIsLoading(false);
       } catch (error) {
         console.log("error: ", error);
-        if(error.name !== "AbortError") {
+        if (error.name !== "AbortError") {
           setError(error.message);
         }
-        
       } finally {
         setIsLoading(false);
       }
-    }
+    };
 
     getMovies();
 
     return () => controller.abort();
+  }, [query, pageList, currentPage, setPageList]);
 
-  },[query, pageList, currentPage, setPageList])
-
-  return {movies, isLoading, error}
+  return { movies, isLoading, error };
 }
